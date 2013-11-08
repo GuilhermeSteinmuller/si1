c3793d53acc1ec7b7611d56c9a456fc8b7db63ca
Guillherme Steinmuller Pimentel <guillherme@linux-g942.site>
2013-11-08 01:19:16 -0300

    caso simples resolvido

--- lab1/src/lab1TDD/NumeroPoExtenso.java
+++ lab1/src/lab1TDD/NumeroPoExtenso.java
@@ -3,18 +3,26 @@ package lab1TDD;
 public class NumeroPoExtenso {
 
 	public String[] ZeroANove;
-	
+	public String[] DezADezenove;
 	
 	
 	public NumeroPoExtenso() {
 		
-		ZeroANove = new String[] { "zero","um","dois","tres","quatro","cinco","seis","sete","oito","nove","dez"};	
+		ZeroANove = new String[] { "zero","um","dois","tres","quatro","cinco","seis","sete","oito","nove"};
+		DezADezenove = new String[] {"dez", "onze", "doze", "treze", "catorze",
+                "quinze", "dezesseis", "dezessete", "dezoito", "dezenove" };
 	}
 	
 	
 	
 	public String extenso(int numero){
 	
+		if (numero < 10){
+			return ZeroANove[numero];
+		}else if(numero <= 19){
+			return DezADezenove[numero - 10];
+		}
+		
 		return ZeroANove[numero];
 	}
 	
