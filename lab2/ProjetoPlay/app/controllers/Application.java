0a98e38f5b337fdf9fbb5ad667223b532160bc8f
Guillherme Steinmuller Pimentel <guillherme@linux-g942.site>
2013-11-24 23:24:33 -0300

    redirecionando tasks

--- lab2/ProjetoPlay/app/controllers/Application.java
+++ lab2/ProjetoPlay/app/controllers/Application.java
@@ -8,11 +8,24 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok("Hello world");
+        return redirect(routes.Application.tasks());
     }
 
     public static Result helloWorld() {
         return ok(helloWorld.render("Hello world",1));
 }  
+    
+    public static Result tasks(){
+    	return TODO;
+    }
+    
+    public static Result newTask(){
+    	return TODO;
+    }
+    
+    public static Result deleteTask(Long id){
+    	return TODO;
+    }
+    
 
 }
